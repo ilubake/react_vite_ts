e2d34d8e478b85ceb87cfd4dff48ad1c35c07f49
username <i_lubake@163.com>
2024-04-15 00:01:31 +0800

    侧边栏绑定路由信息

--- src/views/Layout/aside.tsx
+++ src/views/Layout/aside.tsx
@@ -1,30 +1,10 @@
 import React, { useState } from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
-
+import route_items from "../../routes/routeConfig";
 // 定义侧边栏组件
 const Aside: React.FC = () => {
-  const [asideData,] = useState<MenuProps['items']>([
-    {
-      label: '首页',
-      key: 'home',
-      icon: <MailOutlined />,
-      // children: [],
-    },
-    {
-      label: '控制台',
-      key: 'layout',
-      icon: <AppstoreOutlined />,
-      children: [
-        {
-          label: '用户',
-          key: 'user',
-          icon: <SettingOutlined />,
-        },
-      ],
-    },
-  ]);
+  const [asideData,] = useState<MenuProps['items']>(route_items);
 
   const onClick: MenuProps['onClick'] = (e) => {
     console.log('点击：', e);
